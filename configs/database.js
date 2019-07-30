9844abc69d1611dd9dabff0915616851743a6594
DollarAkshay <akshay95aradhya@gmail.com>
2019-07-30 22:03:12 +0530

    Adding Config Folder

---
+++ configs/database.js
@@ -0,0 +1,8 @@
+module.exports.config = {
+    uri: 'mongodb://admin:iKWtCLsiCj57Ujr@ds115193.mlab.com:15193/classroom-doubt?retryWrites=true',
+    
+    options: {
+        dbName: 'classroom-doubt',
+        useNewUrlParser: true
+    },
+}
